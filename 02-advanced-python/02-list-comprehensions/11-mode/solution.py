56b1ee7cb42f7920d1998acf3ffbbd91f7adb04b
stuke82 <47459076+stuke82@users.noreply.github.com>
2019-03-04 16:45:31 +0100

    Update solution.py
    better cause shorter :p

--- 02-advanced-python/02-list-comprehensions/11-mode/solution.py
+++ 02-advanced-python/02-list-comprehensions/11-mode/solution.py
@@ -1,19 +1,11 @@
 def frequencies(ns):
     result = {}
-
     for n in ns:
         if n not in result:
             result[n] = 0
         result[n] += 1
-
     return result
-
-
-def second(pair):
-    return pair[1]
-
 
 def mode(ns):
     fs = frequencies(ns)
-
-    return max(fs.items(), key=second)[0]
+    return max(fs.items(), key=lambda el: el[1])[0]
