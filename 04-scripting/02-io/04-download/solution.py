f7adf65ee3bdb8eb365737a68cac20103f31c1a1
stuke82 <47459076+stuke82@users.noreply.github.com>
2019-03-16 21:44:39 +0100

    Update solution.py
    Didnt match with explanations.md; alternatively that file can be changed as well :-) (code went looking for a word on dictionary.com site)

--- 04-scripting/02-io/04-download/solution.py
+++ 04-scripting/02-io/04-download/solution.py
@@ -1,9 +1,8 @@
 import urllib.request
 import sys
 
-
 word = sys.argv[1]
 
-with urllib.request.urlopen(f"https://www.dictionary.com/browse/{word}") as input:
+with urllib.request.urlopen(f"{word}") as input:
     contents = input.read()
     print(contents)
